35a6dfce643b426ab85be7b365cfe19b2dd7ee76
maiconlmotadev <maiconlmotadev@hotmail.com>
2021-11-13 17:53:22 +0000

    Iniciando projeto

---
+++ Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Maicon Mota on 13/11/2021.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
